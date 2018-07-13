5bd775d9024d452a2fc2153b68b98d7bb33e1ee7
Ashish Jain <ashishjain030495@live.com>
2018-07-13 23:35:33 +0530

    myshell code with cd

---
+++ System_Prog/myshell/myshell.c
@@ -0,0 +1,67 @@
+//Ashish Jain MT18052
+//Sarosh Hasan MT18084
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+#define MAX_BUFFER 150
+int main()
+{
+    int i = 0,
+        j = 0,
+        nbytes = 0,
+        pid = 1,
+        status,
+        fd[2];
+    char readbuffer[MAX_BUFFER] = "",
+         *token = NULL,
+         command[MAX_BUFFER] = "";
+    do
+    {
+        pipe(fd);
+        /*creates  a pipe, a unidirectional data channel that can be used
+          for interprocess communication.*/
+
+        pid = fork();//create a new child process
+        if(pid == 0)
+        {
+            char *commands[10];
+            printf("@> ");
+            char tmp[10] = "/bin/sh"; //first command to execute command
+            char tmp2[5] = "-c"; // parameter
+            fgets(command, MAX_BUFFER, stdin); // taking input command from user
+            commands[i++] = tmp;
+            commands[i++] = tmp2;
+            commands[i++] = command;
+            commands[i++] = NULL;
+            if(strstr(command,"cd") || strstr(command,"exit")) // checking if command to be executed is cd or exit
+            {
+                close(fd[0]);//closing the read end of pipe
+                write(fd[1], command, (strlen(command)+1)); // passing command if cd or exit to parent process
+            }
+            int ret_val = execv(commands[0],(commands)); // executing commands 
+            exit(0); // exiting child process
+        }
+        else
+        {
+            close(fd[1]);//closing the write end of pipe
+            nbytes = read(fd[0], readbuffer, sizeof(readbuffer)); // reading message from pipe received fom child process
+            wait(&status); // waiting for child process to complete execution
+            if(nbytes > 0)
+            {
+                token = strtok(readbuffer," \n");
+                if(strcmp(token,"cd") == 0)
+                {
+                    token = strtok(NULL," \n");
+                    chdir(token); // changing path to path received from child process in second token
+                }
+                else if(strcmp(token,"exit") == 0)
+                {
+                    break; // exiting from infinite loop if exit received from child process
+                }
+            }
+        }
+    }while(strcmp(readbuffer,"exit") != 0);
+    return 0;
+}
---
+++ System_Prog/myshell/readme.txt
@@ -0,0 +1,8 @@
+NAME: Ashish Jain, Sarosh Hasan
+Roll No.: MT18052, MT18084
+
+This Program is an example of how to create and run your own shell in C.
+
+To run myshell
+1. $ make
+2  $ ./myshell.exe
